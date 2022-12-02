44deeb5bad1f4fe91f4c5769904b7375d5b9273a
Iqbalalfarizi <iqbal2905h@gmail.com>
2022-12-02 16:51:39 +0700

    Update Cart Item

--- components/cart/CartItem.js
+++ components/cart/CartItem.js
@@ -1,4 +1,4 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, Image } from "react-native";
 import React from "react";
 import { cartActions } from "../../store/cart-slice";
 import { useDispatch } from "react-redux";
@@ -15,17 +15,31 @@ const CartItem = ({ item }) => {
     dispatch(cartActions.addItemTocart({ id, title, price }));
   };
   return (
-    <View className="p-5">
-      <Text>{title}</Text>
-      <Text>{quantity}</Text>
-      <Text>{total}</Text>
-      <Text>{price}</Text>
-      <Pressable>
-        <Text onPress={removeItemHandler}>-</Text>
-      </Pressable>
-      <Pressable>
-        <Text onPress={addItemHandler}>+</Text>
-      </Pressable>
+    <View className="flex flex-row p-3 space-x-3 bg-red-400 ">
+      <View className="">
+        <Image
+          className="w-20 h-20 rounded-md "
+          source={{
+            uri: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80",
+          }}
+        />
+      </View>
+      <View className="flex flex-col ">
+        <Text className="text-2xl">{title}</Text>
+        <Text className="text-lg">RP {price}</Text>
+        <View className="flex flex-row">
+          <Text className="text-lg">Total : {total}</Text>
+          <View className="flex flex-row bg-slate-400">
+            <Pressable onPress={removeItemHandler}>
+              <Text className="text-2xl px-1  bg-green-800">-</Text>
+            </Pressable>
+            <Text className="text-2xl px-1">{quantity}</Text>
+            <Pressable onPress={addItemHandler}>
+              <Text className="text-2xl px-1 bg-green-800">+</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
